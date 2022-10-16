d4005edb4d93b3d40ee0999d97cb8fd7bcbd0900
Jhacoby K. Delgado <jkeithd28@gmail.com>
2022-10-16 02:25:28 -0300

    page built by js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-const navBar = document.createElement('nav');
-
-const uList = document.createElement('ul');
-
-const getProducts = async() => {
+const getProducts = async(n) => {
 
       try {
         
-        const resp = await fetch('http://localhost:3000/products/1');
+        const resp = await fetch(`http://localhost:3000/products/${n}`);
 
         if (resp.ok) {
             const data = await resp.json();
@@ -43,12 +39,12 @@ const getProducts = async() => {
               cardPrice.classList.add(...cardPriceClasses);
               const price = document.createElement('span');
               price.classList.add('text-dark');
-              price.innerHTML = `${dat.price}`;
+              price.innerHTML = `${dat.price} CLP`;
               cardPrice.appendChild(price);
               cardPrices.appendChild(cardPrice);
               const discount = document.createElement('div');
               discount.classList.add('font-weight-bold');
-              discount.innerHTML = `${dat.discount}%`
+              discount.innerHTML =  dat.discount ? `${dat.discount}% off` : '';
               cardPrices.appendChild(discount);
               cardInfo.appendChild(cardPrices);
               card.appendChild(cardInfo)
@@ -71,4 +67,87 @@ const getProducts = async() => {
 
 }
 
-getProducts();
+const getCategories = async() => {
+
+  try {
+    
+    const resp = await fetch('http://localhost:3000/category');
+
+    if (resp.ok) {
+        const data = await resp.json();
+
+        const root = document.querySelector('.root');
+        const divNavClasses = ['d-flex', 'flex-column', 'flex-shrink-0', 'p-3', 'text-white', 'bg-dark']
+        const divNav = document.createElement('div');   
+        divNav.classList.add(...divNavClasses);     
+        const nav = document.createElement('nav');
+        const ulClasses = ['nav','nav-pills','flex-colum','mb-auto']
+        const ul = document.createElement('ul');
+        ul.classList.add(...ulClasses);
+
+        for (let dat of data) {
+          const li = document.createElement('li');          
+          const aClasses = dat === data[0] ? ['nav-link','active'] : ['nav-link','text-white'] ;
+          const a = document.createElement('a');
+          a.classList.add(...aClasses);
+          a.id = `${dat.id}`;
+          const aTxt = dat.name.toUpperCase();
+          a.innerHTML = aTxt;
+          li.appendChild(a);
+          ul.appendChild(li);
+        }
+
+        nav.appendChild(ul);
+        divNav.appendChild(nav);
+        root.appendChild(divNav);
+
+      } else {
+        throw await resp.json();
+    }
+
+  } catch (error) {
+
+    throw error;
+
+  }      
+
+}
+
+const getSearcher = () => {
+  const root = document.querySelector('.root');
+  const header = document.createElement('header');
+  const form =  document.createElement('form');
+  const divForm = document.createElement('div');
+  divForm.classList.add('input-group');
+  const divInput = document.createElement('div');
+  divInput.classList.add('form-outline');
+  const input = document.createElement('input');
+  input.type = 'text'; input.id = 'form1'; 
+  input.placeholder = 'Search';
+  input.classList.add('form-control');
+  divInput.appendChild(input);
+  divForm.appendChild(divInput);
+  const button = document.createElement('button');
+  button.type = 'submit'; 
+  const btnClasses = ['btn','btn-primary'];
+  button.classList.add(...btnClasses);
+  const i = document.createElement('i');
+  const iClasses = ['fas','fa-search'];
+  i.classList.add(...iClasses);
+  button.appendChild(i);
+  divForm.appendChild(button);
+  form.appendChild(divForm);
+  header.appendChild(form);
+  root.appendChild(header);  
+}
+
+
+const init = () => {
+    
+    getSearcher();
+    getCategories();
+    getProducts('2');
+
+}
+
+init();
